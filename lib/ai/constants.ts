d8c14b21f51660ce8b2bc70dea0358fdbe9c11ff
Colanns <mycolands@gmail.com>
2025-11-14 01:02:26 +0800

    fix: 修复 kourichat 不可用的问题

--- lib/ai/constants.ts
+++ lib/ai/constants.ts
@@ -64,6 +64,7 @@ export const AI_PROVIDER_CATALOG: AIProviderOption[] = [
         docsUrl: 'https://api.kourichat.com/register?aff=mahoshojo',
         baseUrl: 'https://api.kourichat.com/v1',
         type: 'openai',
+        mode: 'json',
         models: [
             {
                 value: 'gemini-2.5-pro',
@@ -76,11 +77,6 @@ export const AI_PROVIDER_CATALOG: AIProviderOption[] = [
                 description: '按次计费，场景和人数或生成字数多的时候选用此模型性价比更高哦！'
             },
             {
-                value: 'gemini-2.5-auto',
-                label: 'Gemini 2.5 Auto',
-                description: '智能路由模型，将根据提示词难度与供需关系等多种因素在 Gemini-2.5 系列模型内路由。性价比较高，推荐使用。'
-            },
-            {
                 value: 'gemini-2.5-flash',
                 label: 'Gemini 2.5 Flash',
                 description: 'Google 旗下的最先进模型系列，在性能和价格上十分均衡，也是魔法少女生成器默认使用的模型。'
@@ -90,31 +86,31 @@ export const AI_PROVIDER_CATALOG: AIProviderOption[] = [
                 label: 'Gemini 2.5 Flash Lite',
                 description: 'Google 旗下的最先进模型系列，性能略差但是速度很快，是魔法少女生成器默认使用的轻量模型。'
             },
-            {
-                value: 'glm-4.6',
-                label: 'GLM-4.6',
-                description: '智谱旗下的大模型。'
-            },
-            {
-                value: 'deepseek-v3.2-exp',
-                label: 'DeepSeek V3.2 Exp',
-                description: 'DeepSeek 最新版本。'
-            },
-            {
-                value: 'deepseek-r1',
-                label: 'DeepSeek R1',
-                description: 'DeepSeek 思考版本。'
-            },
-            {
-                value: 'kimi-k2',
-                label: 'Kimi K2',
-                description: 'Moonshot 旗下的大模型，可以看出我懒得写描述了。'
-            },
-            {
-                value: 'doubao-seed-1-6',
-                label: 'Doubao Seed 1.6',
-                description: '怎么还有豆包（暂不稳定，不推荐使用）'
-            },
+            // {
+            //     value: 'glm-4.6',
+            //     label: 'GLM-4.6',
+            //     description: '智谱旗下的大模型。'
+            // },
+            // {
+            //     value: 'deepseek-v3.2-exp',
+            //     label: 'DeepSeek V3.2 Exp',
+            //     description: 'DeepSeek 最新版本。'
+            // },
+            // {
+            //     value: 'deepseek-r1',
+            //     label: 'DeepSeek R1',
+            //     description: 'DeepSeek 思考版本。'
+            // },
+            // {
+            //     value: 'kimi-k2',
+            //     label: 'Kimi K2',
+            //     description: 'Moonshot 旗下的大模型，可以看出我懒得写描述了。'
+            // },
+            // {
+            //     value: 'doubao-seed-1-6',
+            //     label: 'Doubao Seed 1.6',
+            //     description: '怎么还有豆包（暂不稳定，不推荐使用）'
+            // },
         ]
     },
     {
